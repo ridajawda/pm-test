8d1eef2cf38a006bbbed3932a2c9d476b5fdbf4a
Rida <ridajawda@gmail.com>
2018-06-28 01:28:45 -0500

    ver 6.1

---
+++ pm-test/src/main/java/zfp/com/pmtest/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package zfp.com.pmtest.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
--- pm-test/src/main/java/zfp/com/pmtest/controller/LoginController.java
+++ pm-test/src/main/java/zfp/com/pmtest/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 	
-	@GetMapping("/showLogin") 
+	@GetMapping("/showMyLoginPage") 
 	public String myShowLogin () {
 		
 		return "login";
